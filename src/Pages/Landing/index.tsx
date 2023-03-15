2d49a52215ddf2717d09fc818e6a9f7c404dfba9
jake0319 <shjun1411@naver.com>
2023-03-15 21:23:57 +0900

    Feat: 2번째 랜딩 페이지 제작

--- src/Pages/Landing/index.tsx
+++ src/Pages/Landing/index.tsx
@@ -1,4 +1,5 @@
-// import {useEffect, useState } from "react";
+/** @jsxImportSource @emotion/react */
+
 import {
   Iphone,
   IphoneContainer,
@@ -10,21 +11,24 @@ import {
   StartButton,
   StartText,
   Gifticon,
+  LandingBox1,
+  // ArrowIcon
 } from "./styles";
 import { useNavigate} from "react-router-dom";
 import { AnimatePresence, motion } from "framer-motion";
+import {css} from '@emotion/react';
 
 export const HeaderSet = () => {
   const navigate = useNavigate();
   return (
-    <>
-      <Header>
+    <div>
+      <Header onClick={()=>{navigate('/')}}>
         <LogoImg src="/imgs/logo.png"></LogoImg>
         <LoginSkipContainer>
           <LoginButton>
             <div
               onClick={() => {
-                navigate("login");
+                navigate("/login");
                 console.log("/login");
               }}
             >
@@ -46,12 +50,13 @@ export const HeaderSet = () => {
           </SkipButton>
         </LoginSkipContainer>
       </Header>
-    </>
+    </div>
   );
 };
+
+//랜딩 초기화면 컴포넌트 
 const Home = () => {
   //framer-variants
-
   const Variants = {
     hidden: {
       scale: [1, 1.1, 1, 1.1, 1, 1.1, 1, 1.1, 1,1.1,1],
@@ -64,10 +69,9 @@ const Home = () => {
       boxShadow: "0px 0px 100px rgb(250,255,255)",
     },
   };
-
   const navigate = useNavigate();
   return (
-    <>
+    <div>
       <HeaderSet></HeaderSet>
       <div style={{ width: "100%", height: "auto" }}>
         <motion.div
@@ -75,11 +79,11 @@ const Home = () => {
           style={{
             width: "100%",
             height: "800px",
-            background:
+            backgroundColor:
               "linear-gradient(60deg, #F1F3EE 0%, #FFFFFF 100%) 0% 0% no-repeat padding-box",
           }}
         >
-          <AnimatePresence>
+          {/* <AnimatePresence> */}
             <IphoneContainer initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
               <StartText>
                 <div>
@@ -103,7 +107,9 @@ const Home = () => {
                     쉽고 간단하게 기부를 시작하세요
                   </h3>
                 </div>
-                <Gifticon></Gifticon>
+                <Gifticon initial={{y:'17.5rem', translateX:'1rem'}} whileHover={{
+                  x:[-5,5,-5,5],
+                  transition:{ duration: 1, x: {yoyo: 2} }}}></Gifticon>
               </StartText>
               <StartButton
                 variants={Variants}
@@ -120,20 +126,92 @@ const Home = () => {
               </StartButton>
               <Iphone></Iphone>
             </IphoneContainer>
-          </AnimatePresence>
+          {/* </AnimatePresence> */}
         </motion.div>
       </div>
-    </>
+    </div>
   );
 };
 
-export const NextLanding=()=>{return(<>
-<HeaderSet></HeaderSet>
-<motion.div></motion.div>
-<motion.div></motion.div>
-<motion.div></motion.div>
-<motion.div></motion.div>
 
-</>)}
+// '/landing'페이지 컴포넌트
+export const NextLanding=()=>{
+  const HoverVariants = {hover:{scale: 1.07}}
+  return(<div>
+    {/* 헤더컴포넌트 */}
+<HeaderSet></HeaderSet>
+<motion.div css={css`
+background-image:url('/imgs/index_Phone.png');
+background-size: contain;
+background-repeat:no-repeat;
+transform:translateY(-25px);
+background-position: center;
+width: 100%;
+height: 800px;
+position:absolute;
+z-index: 2;
+scale:2.4;	pointer-events: none; 
+`
+}>
+
+</motion.div>
+{/* gradient 백그라운드(flex-container) */}
+<div css={css`width:100vw;
+height: calc(100vh - 134px);
+background: rgb(245,246,242);
+background: linear-gradient(180deg, rgba(245,246,242,1) 0%, rgba(255,255,255,1) 100%);
+display:flex; 
+justify-content:space-around;
+align-items:center;
+flex-wrap:wrap;
+`}>
+
+  {/* 이미지박스 */}
+  <LandingBox1 variants={HoverVariants} whileHover="hover"
+  css={css`background: linear-gradient(0deg, rgba(255,255,255,1) 50%, rgba(234,237,235,1) 50%); z-index:2;
+  `}>
+  <div css={css` margin-top:50px;
+    transform: rotate(15deg) translateX(1rem);
+    width:250px; height:250px;
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-image: url('/imgs/info_1st.png')`}></div>
+      <h1 css={css`font-size:1rem; margin-bottom:50px;
+      font-weight:bold;`}>미사용 기프티콘<span css={{fontWeight:'normal'}}>으로
+      <br></br><h1>따뜻한 마음을 나누세요</h1></span>
+      </h1>
+    </LandingBox1>
+    <LandingBox1 variants={HoverVariants} whileHover="hover"
+    css={css`background: linear-gradient(0deg, rgba(255,255,255,1) 50%, rgba(234,237,235,1) 50%); z-index:2;
+  `}>
+  <div css={css` margin-top:50px;
+    scale: 0.8;
+    transform:translateX(1rem) translateY(-1rem);
+    width:250px; height:250px;
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-image: url('/imgs/info_2nd.png')`}></div>
+      <h1 css={css`font-size:1rem; margin-bottom:50px;
+      font-weight:bold;`}>미사용 기프티콘<span css={{fontWeight:'normal'}}>으로
+      <br></br><h1>따뜻한 마음을 나누세요</h1></span>
+      </h1>
+    </LandingBox1>
+    <LandingBox1 variants={HoverVariants} whileHover="hover"
+    css={css`background: linear-gradient(0deg, rgba(255,255,255,1) 50%, rgba(234,237,235,1) 50%); z-index:2;
+  `}>
+  <div css={css` margin-top:50px;
+    scale: 1.1;
+    transform:translateY(-2rem) translateX(-2.5rem);
+    width:250px; height:250px;
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-image: url('/imgs/Rock_Sprit.png')`}></div>
+      <h1 css={css`font-size:1rem; margin-bottom:50px;
+      font-weight:bold;`}>미사용 기프티콘<span css={{fontWeight:'normal'}}>으로
+      <br></br><h1>따뜻한 마음을 나누세요</h1></span>
+      </h1>
+    </LandingBox1>
+    </div>
+</div>)}
 
 export default Home;
